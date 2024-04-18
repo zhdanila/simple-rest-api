a6a73204b7608202b4873ea392a56eea0624bd3c
Danya Zhurba <daniazhurba2004@gmail.com>
2024-04-18 12:41:25 +0300

    Add repository and connect database

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
 	todo_list "todo-list"
 	"todo-list/internal/handler"
+	"todo-list/internal/repository"
 )
 
-//set endpoints
 //connect database
 //create repository
 //create service
@@ -14,10 +16,31 @@ import (
 //init dependency injection
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error with config file: %s", err.Error())
+	}
+
+	_, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("error with connecting to database: %s", err.Error())
+	}
 	handlers := new(handler.Handler)
 	srv := new(todo_list.Server)
 
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil { // Change port to match your configuration
 		log.Fatalf("error with running server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
